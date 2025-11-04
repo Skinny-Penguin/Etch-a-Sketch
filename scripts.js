ed9a7cf18f28d7df48119278cb68d83361f5801c
Liberal Neupane <parbhakharneupane@gmail.com>
2025-11-04 12:19:19 +0545

    commiting a lot of changes

--- scripts.js
+++ scripts.js
@@ -1,31 +1,69 @@
 const container = document.querySelector(".container");
-createBoxes(40);
-
-function createBoxes(size){
-    for(let i=1; i<= 256 ; i++){
-        const box = document.createElement("div");
-        box.setAttribute("class", "box");
-        box.style.width= `${size}px`;
-        box.style.height = `${size}px`;
-        container.appendChild(box);
-    }
+var width = container.width;
+createBoxes(40, width);
+var Rand = true;
+var Black = false;
+var Erase = false;
+function randClick(){
+ Rand = true;
+Black = false;
+Erase = false;   
+}
+function blackClick(){
+ Rand = false;
+Black = true;
+Erase = false;   
+}
+function eraseClick(){
+ Rand = false;
+Black = false;
+Erase = true;   
+}
+function resizeClick(){
+    var row = parseInt(prompt("How many pixels do you want in one row/column?"));
+    createBoxes(row, width);
+}
+function createBoxes(size,width) {
+    var limit = width/size;
+  for (let i = 1; i <= limit*limit; i++) {
+    const box = document.createElement("div");
+    box.setAttribute("class", "box");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    container.appendChild(box);
+  }
 }
 const boxes = document.querySelectorAll(".box");
-boxes.forEach(box => {
-    box.addEventListener("mouseover", () => {
-        box.style.background = RandomColor;
-        box.style.transform = "scale(1.1)";
-        box.style.transition = "all 0.3s ease";
-    });
-    
-    box.addEventListener("mouseout", () => {
-        box.style.transform = "scale(1)";
-    });
-});
-function RandomColor(){
-    var red = 255*Math.random();
-    var blue = 255*Math.random();
-    var green = 255*Math.random();
-    return `rgb(${red},${blue},${green})`
+boxes.forEach((box) => {
+  box.addEventListener("mouseover", () => {
+    box.style.background = Whichcolour();
+    box.style.transform = "scale(1.1)";
+    box.style.transition = "all 0.3s ease";
+  });
 
+  box.addEventListener("mouseout", () => {
+    box.style.transform = "scale(1)";
+  });
+});
+function Whichcolour() {
+  if ((Rand == true)) {
+    return RandomColor();
+  }
+  else if( (Black ==true)){
+    return `rgb(0,0,0)`;
+  }
+  else if((Erase==true)){
+    return Eraser();
+  }
 }
+function RandomColor() {
+  var red = 255 * Math.random();
+  var blue = 255 * Math.random();
+  var green = 255 * Math.random();
+  return `rgb(${red},${blue},${green})`;
+}
+function Eraser(){
+    var bgColor = container.style.background;
+    return bgColor;
+}
+
